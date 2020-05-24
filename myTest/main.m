890d5c2d85b5ddacae3ea4013dbce53d936800bb
刘立超 <1771617585@qq.com>
2020-05-24 12:28:19 +0800

    Build ok

---
+++ myTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  myTest
+//
+//  Created by 刘立超 on 2020/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        [NSObject alloc];
+    }
+    return 0;
+}
